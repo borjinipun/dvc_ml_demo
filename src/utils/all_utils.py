6628f4cf201ee85e021ea809c5d6ed5640864a3e
borjinipun <borjinipun@gmail.com>
2022-03-20 12:13:51 +0530

    setup file is staged

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -4,7 +4,7 @@ import os
 
 
 def read_yaml(path_to_yaml: str) -> dict:
-    with open(path_to_yaml) as yaml_files:
-        content = yaml.safe_load(yaml_files)
+    with open(path_to_yaml) as yaml_file:
+        content = yaml.safe_load(yaml_file)
 
     return content
