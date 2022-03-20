a55573ca18754f7b045aafd4ffb6369e37922c9e
borjinipun <borjinipun@gmail.com>
2022-03-20 13:02:31 +0530

    stage 01 created

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -1,4 +1,3 @@
- 
 import yaml
 import os
 
@@ -8,3 +7,8 @@ def read_yaml(path_to_yaml: str) -> dict:
         content = yaml.safe_load(yaml_file)
 
     return content
+
+def create_directory(dirs: list):
+    for dir_path in dirs:
+        os.makedirs(dir_path, exist_ok=True)
+        print(f"directory is creater at {dir_path}")
